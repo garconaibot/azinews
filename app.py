b6c2100a01c2f391b9d02db8d06a03d837c02a14
GarconBot <garcon@bot.ai>
2026-02-26 11:56:18 +0200

    Revenire categorie sus

--- app.py
+++ app.py
@@ -256,8 +256,8 @@ for i, news in enumerate(news_data):
     col1, col2 = st.columns([8, 2])
     
     with col1:
-        st.markdown(f"**{news['title']}**", unsafe_allow_html=True)
         st.markdown(f"<span class='news-category'>{news['category']}</span>", unsafe_allow_html=True)
+        st.markdown(f"**{news['title']}**")
     
     with col2:
         btn_text = "➖ Ascunde" if st.session_state.expanded_news.get(i) else "➕ Citește tot"
